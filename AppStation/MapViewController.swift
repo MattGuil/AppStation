52bc5eb97a1e2f2568f23d0b223903c6dac9cee7
MattGuil <guillemin.matt@gmail.com>
2024-04-08 13:10:35 +0200

    show user location

--- AppStation/MapViewController.swift
+++ AppStation/MapViewController.swift
@@ -9,24 +9,52 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
     
     let locationManager = CLLocationManager()
     
     // var stations: [[String: Any]] = []
-    @IBOutlet weak var map: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // loadDataFromAPI()
         
+        // Demander l'autorisation pour l'utilisation de la localisation
         locationManager.requestWhenInUseAuthorization()
+        
+        // Définir le délégué de CLLocationManager
+        locationManager.delegate = self
+        
+        // Configuration de la précision de la localisation
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        // Commencer à mettre à jour la localisation
         locationManager.startUpdatingLocation()
         
+        // Afficher la localisation de l'utilisateur sur la carte
         map.showsUserLocation = true
+    }
+    
+    // Cette méthode est appelée chaque fois que la position de l'utilisateur est mise à jour
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Récupérer la dernière position de l'utilisateur
+        guard let userLocation = locations.last else {
+            return
+        }
+        
+        // Mettre à jour la région de la carte pour inclure la nouvelle position de l'utilisateur
+        let region = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        map.setRegion(region, animated: true)
+    }
+    
+    // Gérer les erreurs de localisation
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error requesting location: \(error.localizedDescription)")
     }
     
     func loadDataFromAPI() {
@@ -60,7 +88,31 @@ class MapViewController: UIViewController {
             // récupération des données JSON
             do {
                 if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                    print(json)
+                    if let records = json["results"] as? [[String: Any]] {
+
+                        var stationCounter = 0
+
+                        // parcours de toutes les stations services enregistrées dans l'API
+                        for record in records {
+                            // récupération des coordonnées géographiques de la station courante
+                            if let geo_point = record["geo_point"] as? [String: Double] {
+
+                                // création d'une annotation de marqueur pour la station courante
+                                let annotation = MKPointAnnotation()
+                                annotation.coordinate = CLLocationCoordinate2D(latitude: geo_point["lat"] ?? 0.0, longitude: geo_point["lon"] ?? 0.0)
+
+                                // ajout de l'annotation à la carte
+                                DispatchQueue.main.async {
+                                    self.map.addAnnotation(annotation)
+                                }
+                            }
+
+                            stationCounter += 1
+                        }
+
+                        print("stationCounter = \(stationCounter)")
+
+                    }
                 }
             } catch {
                 print("Erreur lors de la conversion JSON : \(error)")
@@ -72,7 +124,7 @@ class MapViewController: UIViewController {
     }
 
     /*
-     func placeNearestStationMarker(userLocation: CLLocation) {
+    func placeNearestStationMarker(userLocation: CLLocation) {
          var nearestStation: [String: Any]? = nil
          var nearestDistance: CLLocationDistance = Double.infinity
 
@@ -96,15 +148,14 @@ class MapViewController: UIViewController {
              // Ajouter un marqueur pour la station la plus proche sur la carte
              if let latitude = (nearestStation["geo_point"] as? [String: Double])?["lat"],
                 let longitude = (nearestStation["geo_point"] as? [String: Double])?["lon"],
-                let stationName = nearestStation["station_name"] as? String {
+                let stationName = nearestStation["name"] as? String {
                  let annotation = MKPointAnnotation()
                  annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
                  annotation.title = stationName
-                 map.addAnnotation(annotation)
+                 self.map.addAnnotation(annotation)
              }
          }
      }
      */
-
 
 }
